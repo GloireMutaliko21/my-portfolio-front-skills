c9f378f32ba89ae65b41b6a250b4583ae86d5097
Gloire Mutaliko <mufunyig@gmail.com>
2023-08-03 18:41:00 +0200

    feat: init internalization with i18n

---
+++ app/i18n/client.ts
@@ -0,0 +1,49 @@
+'use client';
+
+import i18next from 'i18next';
+import { useEffect } from 'react';
+import {
+	initReactI18next,
+	useTranslation as useTranslationOrg,
+} from 'react-i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
+// import LocizeBackend from 'i18next-locize-backend'
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { getOptions, languages } from './settings';
+
+const runsOnServerSide = typeof window === 'undefined';
+
+// on client side the normal singleton is ok
+i18next
+	.use(initReactI18next)
+	.use(LanguageDetector)
+	.use(
+		resourcesToBackend(
+			(language: any, namespace: any) =>
+				import(`./locales/${language}/${namespace}.json`)
+		)
+	)
+	// .use(LocizeBackend) // locize backend could be used on client side, but prefer to keep it in sync with server side
+	.init({
+		...getOptions(),
+		lng: undefined, // let detect the language on client side
+		detection: {
+			order: ['path', 'htmlTag', 'navigator', 'cookie'],
+		},
+		preload: runsOnServerSide ? languages : [],
+	});
+
+export function useTranslation(lng: any, ns: any, options: any) {
+	const ret = useTranslationOrg(ns, options);
+	const { i18n } = ret;
+	if (runsOnServerSide && i18n.resolvedLanguage !== lng) {
+		i18n.changeLanguage(lng);
+	} else {
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		useEffect(() => {
+			if (i18n.resolvedLanguage === lng) return;
+			i18n.changeLanguage(lng);
+		}, [lng, i18n]);
+	}
+	return ret;
+}
