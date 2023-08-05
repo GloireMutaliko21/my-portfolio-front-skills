933779e04d635a26b041673c302c6959e80e13b6
Gloire Mutaliko <mufunyig@gmail.com>
2023-08-05 11:16:14 +0200

    style: init global css

--- app/[lng]/components/Footer.tsx
+++ app/[lng]/components/Footer.tsx
@@ -4,7 +4,11 @@ import { useTranslation } from '@/app/i18n/client';
 
 const Footer = ({ lng }: any) => {
 	const { t } = useTranslation(lng, 'footer', {});
-	return <footer>Footer</footer>;
+	return (
+		<footer className='py-5 xl:py-10 w-full mt-16 bg-black text-white px-2 md:px-10 lg:px-16 xl:px-20 2xl:px-72'>
+			Footer
+		</footer>
+	);
 };
 
 export default Footer;
