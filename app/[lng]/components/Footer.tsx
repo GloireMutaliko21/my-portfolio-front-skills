666cc3ea1ee2a0fc9b92a168799a6f9a190008d3
Gloire Mutaliko <mufunyig@gmail.com>
2023-08-05 14:08:12 +0200

    style: responsive  newsletter section

--- app/[lng]/components/Footer.tsx
+++ app/[lng]/components/Footer.tsx
@@ -6,19 +6,19 @@ const Footer = ({ lng }: any) => {
 	const { t } = useTranslation(lng, 'footer', {});
 	return (
 		<footer className='py-5 xl:py-10 w-full mt-16 bg-[#0f172a] text-white px-2 md:px-10 lg:px-16 xl:px-20 2xl:px-72 lg:h-screen 2xl:h-[75vh]'>
-			<section className='py-10 -translate-y-[80%] px-2 md:px-6 lg:px-14 mt-12 rounded-2xl flex flex-wrap justify-between items-center bg-[url("/images/newsletter-bg.webp")] bg-cover'>
-				<div className='max-w-[50%] '>
+			<section className='py-14 -translate-y-[80%] px-5 lg:px-14 mt-12 rounded-2xl flex flex-wrap justify-between items-center gap-y-10 bg-[url("/images/newsletter-bg.webp")] bg-cover text-center lg:text-start'>
+				<div className='max-w-full lg:max-w-[50%] px-8'>
 					<p className='font-bold text-3xl md:text-5xl !leading-tight'>
 						Subscribe to my newsletter
 					</p>
-					<p className='mt-6 leading-relaxed'>
+					<p className='mt-6 leading-relaxed text-sm md:text-base'>
 						Join me and receive daily updates on opportunities to boost your
 						business and effectively manage your activities, whatever they may
 						be.
 					</p>
 				</div>
-				<div className='p-16 min-w-[50%] h-full'>
-					<div className='glass-effetct shadow p-1 rounded-full flex gap-x-2 justify-between border-white/30 border-[0.2px]'>
+				<div className='min-w-[50%] w-full lg:w-1/2 h-full'>
+					<div className='glass-effetct shadow p-1 rounded-full flex gap-x-2 justify-between border-white/30 border-[0.2px] w-full'>
 						<input
 							type='text'
 							placeholder='Entrer your email address'
